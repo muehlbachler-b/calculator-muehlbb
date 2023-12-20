43f3b548d66ba2e120344009a9e85615a516c08a
muehlbachler-b <muehlbachler.b@gmail.com>
2023-12-20 18:24:22 +0100

    change py

--- src/test.py
+++ src/test.py
@@ -6,4 +6,6 @@ from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 @cocotb.test()
 async def test_calculator(dut):
     dut._log.info("start")
+    dut._log.info("reset")
+    dut.rst_n.value = 0
 
