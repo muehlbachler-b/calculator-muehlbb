61091ee9f1e2041c0a87b4c9776c0213262cc465
muehlbachler-b <muehlbachler.b@gmail.com>
2023-12-20 18:34:24 +0100

    comment evthing in py-file

--- src/test.py
+++ src/test.py
@@ -1,11 +1,11 @@
-import cocotb
-from cocotb.clock import Clock
-from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
+#import cocotb
+#from cocotb.clock import Clock
+#from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 
 
-@cocotb.test()
-async def test_calculator(dut):
-    dut._log.info("start")
-    dut._log.info("reset")
-    dut.rst_n.value = 0
+#@cocotb.test()
+#async def test_calculator(dut):
+#    dut._log.info("start")
+#    dut._log.info("reset")
+#    dut.rst_n.value = 0
 
